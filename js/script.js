faaeb69f75955ec8d0406d5675ab6f0a6555c1be
Shrike7 <egor04kotss@gmail.com>
2021-08-06 15:42:12 +0300

    Update js

--- js/script.js
+++ js/script.js
@@ -11,50 +11,146 @@ let levelPath = [],
     levelMemoAmountSleep = 1, //while levelMemoAmountSleep < x levelMemoAmount doesn't increase
     fieldSize = 3;
 
-function gameStart () {
-    gameStartBtn.style.display = 'none';
-    levelLabel.style.display = 'block';
+const gameControl = {
+    gameStart() {
+        gameStartBtn.style.display = 'none';
+        levelLabel.style.display = 'block';
+    
+        levelControl.generateLevelPath();
+        setTimeout(() => levelControl.startLevel(), 500);
+    },
+    gameRestart() {
+        menuBtn.style.display = 'none';
+        gameRestartBtn.style.display = 'none';
+    
+        gameControl.gameReset();
+        levelControl.generateLevelPath();
+        setTimeout(() => levelControl.startLevel(), 500);
+    },
+    userPlay(e) {
+        let userCell = e.target;
+        let userCellPossition = +userCell.getAttribute('data-position');
 
-    generateLevelPath();
-    setTimeout(() => startLevel(), 500);
+        //is user right
+        if (levelPath.includes(userCellPossition) || userCell.classList.contains('active')) {
+            userCell.classList.add('active');
+    
+            let index = levelPath.indexOf(userCellPossition);
+            if (index !== -1) levelPath.splice(index, 1);
+    
+            //is all right
+            if (levelPath.length == 0) {
+                cellGroup.forEach(cellElem => cellElem.removeEventListener('click', this.userPlay));
+                setTimeout(() => levelControl.nextLevel(), 1000);
+            }
+        } else {
+            userCell.classList.add('wrong');
+            gameControl.gameOver();
+        }
+    },
+    gameOver() {
+        cellGroup.forEach(cellElem => cellElem.removeEventListener('click', this.userPlay));
+    
+        levelPath.forEach(cellPosition => {
+            const cellElem = cellGroup[cellPosition];
+            cellElem.classList.add('active');
+        });
+    
+        setTimeout(() => {
+            menuBtn.style.display = 'block';
+            gameRestartBtn.style.display = 'block';
+        }, 500);
+    },
+    gameReset() {
+        //Reset
+        levelPath = [];
+        level = 1;
+        levelMemoAmount = 2;
+        levelMemoAmountSleep = 1;
+        fieldSize = 3;
+    
+        //Reset field
+        cellGroup.forEach(cellElem => {
+            cellElem.classList.remove('active');
+            cellElem.classList.remove('wrong');
+        });
+    
+        //Reset label
+        levelLabel.textContent = level + ' level';
+    
+        createField(fieldSize);
+    },
+    backToMenu() {
+        gameControl.gameReset();
+    
+        menuBtn.style.display = 'none';
+        gameRestartBtn.style.display = 'none';
+        levelLabel.style.display = 'none';
+        gameStartBtn.style.display = 'block';
+    },
 }
 
-function gameRestart () {
-    menuBtn.style.display = 'none';
-    gameRestartBtn.style.display = 'none';
-
-    gameReset();
-    generateLevelPath();
-    setTimeout(() => startLevel(), 500);
-}
-
-function gameReset () {
-    //Reset
-    levelPath = [];
-    level = 1;
-    levelMemoAmount = 2;
-    levelMemoAmountSleep = 1;
-    fieldSize = 3;
-
-    //Reset field
-    cellGroup.forEach(cellElem => {
-        cellElem.classList.remove('active');
-        cellElem.classList.remove('wrong');
-    });
-
-    //Reset label
-    levelLabel.textContent = level + ' level';
-
-    createField(fieldSize);
-}
-
-function backToMenu () {
-    gameReset();
-
-    menuBtn.style.display = 'none';
-    gameRestartBtn.style.display = 'none';
-    levelLabel.style.display = 'none';
-    gameStartBtn.style.display = 'block';
+const levelControl = {
+    generateLevelPath() {
+        if (levelMemoAmountSleep < 3) levelMemoAmountSleep++;
+            else {
+                levelMemoAmountSleep = 1;
+                levelMemoAmount++;
+            }
+    
+    
+        for (let i = 0; i < levelMemoAmount; i++) {
+            let isCellEmpty = false,
+                randomCell;
+    
+            //anti same cell system
+            while (isCellEmpty == false) {
+                randomCell = Math.ceil(Math.random() * cellGroup.length) - 1;
+                if (!levelPath.includes(randomCell)) isCellEmpty = true;
+            }
+    
+            levelPath.push(randomCell);
+        }
+        
+        if (levelPath.length == Math.floor(cellGroup.length / 2.5)) {
+             // more than 7 size too big for field 
+            if (fieldSize !== 7) {
+                levelPath = [];
+                levelMemoAmount = 2;
+    
+                createField(++fieldSize);
+                this.generateLevelPath();
+            } else {
+                levelMemoAmount--;
+            }
+        }
+    },
+    startLevel() {
+        levelPath.forEach(cellPosition => {
+            const cellElem = cellGroup[cellPosition];
+            cellElem.classList.add('active');
+        });
+    
+        setTimeout(() => {
+            levelPath.forEach(cellPosition => {
+                const cellElem = cellGroup[cellPosition];
+                cellElem.classList.remove('active');
+            });
+    
+            cellGroup.forEach(cellElem => cellElem.addEventListener('click', gameControl.userPlay));
+        }, 4000);
+    },
+    nextLevel() {
+        level++;
+        levelLabel.textContent = level + ' level';
+    
+        cellGroup.forEach(cellElem => {
+            cellElem.classList.remove('active');
+        });
+    
+        this.generateLevelPath();
+        this.startLevel();
+    },
 }
 
 function createField (fieldSize) {
@@ -71,106 +167,12 @@ function createField (fieldSize) {
     cellGroup = document.querySelectorAll('#field .cell');
 }
 
-function generateLevelPath () {
-    if (levelMemoAmountSleep < 3) levelMemoAmountSleep++;
-        else {
-            levelMemoAmountSleep = 1;
-            levelMemoAmount++;
-        }
 
-
-    for (let i = 0; i < levelMemoAmount; i++) {
-        let isCellEmpty = false,
-            randomCell;
-
-        //anti same cell system
-        while (isCellEmpty == false) {
-            randomCell = Math.ceil(Math.random() * cellGroup.length) - 1;
-            if (!levelPath.includes(randomCell)) isCellEmpty = true;
-        }
-
-        levelPath.push(randomCell);
-    }
-    
-    if (levelPath.length == Math.floor(cellGroup.length / 2.5)) {
-         // more than 7 size too big for field 
-        if (fieldSize !== 7) {
-            levelPath = [];
-            levelMemoAmount = 2;
-
-            createField(++fieldSize);
-            generateLevelPath();
-        } else {
-            levelMemoAmount--;
-        }
-    }
+function init () {
+    createField(fieldSize);
+    gameStartBtn.addEventListener('click', gameControl.gameStart);
+    gameRestartBtn.addEventListener('click', gameControl.gameRestart)
+    menuBtn.addEventListener('click', gameControl.backToMenu);
 }
 
-function startLevel () {
-    levelPath.forEach(cellPosition => {
-        const cellElem = cellGroup[cellPosition];
-        cellElem.classList.add('active');
-    });
-
-    setTimeout(() => {
-        levelPath.forEach(cellPosition => {
-            const cellElem = cellGroup[cellPosition];
-            cellElem.classList.remove('active');
-        });
-
-        cellGroup.forEach(cellElem => cellElem.addEventListener('click', userPlay));
-    }, 4000);
-}
-
-function userPlay (e) {
-    let userCell = e.target;
-    let userCellPossition = +userCell.getAttribute('data-position');
-
-    //is user right
-    if (levelPath.includes(userCellPossition) || userCell.classList.contains('active')) {
-        userCell.classList.add('active');
-
-        let index = levelPath.indexOf(userCellPossition);
-        if (index !== -1) levelPath.splice(index, 1);
-
-        //is all right
-        if (levelPath.length == 0) {
-            cellGroup.forEach(cellElem => cellElem.removeEventListener('click', userPlay));
-            setTimeout(() => nextLevel(), 1000);
-        }
-    } else {
-        userCell.classList.add('wrong');
-        gameOver();
-    }
-}
-
-function nextLevel () {
-    level++;
-    levelLabel.textContent = level + ' level';
-
-    cellGroup.forEach(cellElem => {
-        cellElem.classList.remove('active');
-    });
-
-    generateLevelPath();
-    startLevel();
-}
-
-function gameOver () {
-    cellGroup.forEach(cellElem => cellElem.removeEventListener('click', userPlay));
-
-    levelPath.forEach(cellPosition => {
-        const cellElem = cellGroup[cellPosition];
-        cellElem.classList.add('active');
-    });
-
-    setTimeout(() => {
-        menuBtn.style.display = 'block';
-        gameRestartBtn.style.display = 'block';
-    }, 500);
-}
-
-createField(fieldSize);
-gameStartBtn.addEventListener('click', gameStart);
-gameRestartBtn.addEventListener('click', gameRestart)
-menuBtn.addEventListener('click', backToMenu);
+document.addEventListener('DOMContentLoaded', init);
